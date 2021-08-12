0299bb7d2709e4b1fb82e4ef2b58e3eb4969aa82
suberumaiku <suberumaiku@gmail.com>
2021-08-12 22:26:13 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="color = color === 'red' ? 'green' : 'red'">
+      Color is: {{ color }}
+    </button>
+    <kodomo :color="color" @action="action" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Kodomo from './components/Kodomo.vue';
+
+const color = ref('red');
+
+function action(msg) {
+  console.log(msg);
+}
+</script>
+
+<style scoped>
+button {
+  color: v-bind(color);
+}
+</style>
---
+++ src/components/Kodomo.vue
@@ -0,0 +1,21 @@
+<template>子供</template>
+
+<script setup>
+import { defineProps, onMounted } from 'vue';
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: '',
+  },
+});
+
+const emit = defineEmits(['action']);
+
+onMounted(() => {
+  console.log(props.color);
+  emit('action', '野郎');
+});
+</script>
+
+<style></style>
